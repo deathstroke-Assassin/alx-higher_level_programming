e2156c41be204edc674b776bb9cf60db4cee1b76
deathstroke-Assassin <moh2004amed@gmail.com>
2023-11-30 04:12:31 -0800

    gioo

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from add_0 import add
-    sum = add(1, 2)
     a = 1
     b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, sum))
+    print("{:d} + {:d} = {:d}".format(a, b, add(1, 2)))
