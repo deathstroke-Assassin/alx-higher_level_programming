08fcadb9202320e35fbcf7e783b5b437aac227e9
deathstroke-Assassin <moh2004amed@gmail.com>
2023-11-29 18:53:49 -0800

    g

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,9 +3,11 @@ for x in range(1, 101):
     if x == 100:
         print("Buzz")
     else:
-        if x % 3 == 0:
+        if x % 3 == 0 and x % 5 != 0:
             print("Fizz ", end="")
-        elif x % 5 == 0:
+        elif x % 5 == 0 and x % 3 != 0:
             print("Buzz ", end="")
+        elif x % 5 == 0 and x % 3 == 0:
+            print("FizzBuzz ", end="")
         else:
             print("{:d} ".format(x), end="")
