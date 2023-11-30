be084d96572dd3b18566515d0c0cb71b6ceb92d9
deathstroke-Assassin <moh2004amed@gmail.com>
2023-11-29 17:37:21 -0800

    el1

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-for i in range(1, 9):
-    for x in range(0, 10):
-        if i > x:
-            print("{:d}{:d}, ".format(i, x),end="")
-        elif (i == 9 and x == 8) or (i == 8 and x == 9):
+for i in range(0, 10):
+    for x in range(1, 10):
+        if i >= x:
+            continue
+        if i == 8 and x == 9:
             print("{:d}{:d}".format(i, x))
+
+        else:
+            print("{:d}{:d}, ".format(i, x),end="")
