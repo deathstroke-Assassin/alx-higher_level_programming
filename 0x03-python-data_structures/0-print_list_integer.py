f99ed010ec7b4376a4cda6dc3ec904a093e6b6ee
deathstroke-Assassin <moh2004amed@gmail.com>
2023-12-03 02:44:04 -0800

    o

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for my_list in range(len(my_list) + 1):
+    for my_list in range(1, len(my_list) + 1):
         print("{}".format(my_list))
