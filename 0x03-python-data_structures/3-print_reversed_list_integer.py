f165ad379ffbaab9f2c7fa4a2cba4b6207d7d0b2
deathstroke-Assassin <moh2004amed@gmail.com>
2023-12-03 05:00:57 -0800

    gfgfgfgsdsd

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    revlist = my_list
-    revlist.reverse()
-    for x in revlist:
-        print("{:d}".format(x))
+    if isinstance(my_list, list):
+        revlist = my_list
+        revlist.reverse()
+        for x in revlist:
+            print("{:d}".format(x))
