b05ee20d9a1cc740b69093b9e920754edb873cd0
deathstroke-Assassin <moh2004amed@gmail.com>
2023-12-04 07:49:54 -0800

    hc

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,6 @@
-#!/usr/bin/python3
 def no_c(my_string):
-    new_string = ""
-    for idx in my_string:
-        if idx != 'c' and idx != 'C':    
-            new_string += idx
-    return new_string
+    new_str = ""
+    for i in my_string:
+        if i is not 'c' and i is not 'C':
+            new_str += i
+    return (new_str)
