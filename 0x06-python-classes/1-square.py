cf2a5c6056d50eb0d9d2ad246e7c150d855542cd
deathstroke-Assassin <moh2004amed@gmail.com>
2023-12-19 14:53:53 -0800

    cc

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" this module defines a empty class square """
+
+
+class Square:
+    """ empty class for task 0 """
+    def __init__(self. size):
+        """ Initialize class """
+        self.__size = size
