b64144e4bc37c2f67638d97d1111339c92044c77
deathstroke-Assassin <moh2004amed@gmail.com>
2023-12-19 15:06:18 -0800

    vvv

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" this module defines a empty class square """
+
+
+class Square:
+    """ empty class for task 0 """
+    def __init__(self, size):
+        """ Initialize class """
+        if type(size) =! int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+             self.__size = size
