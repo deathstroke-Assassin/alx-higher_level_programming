a94788066418a33ed74586945db405dd449a13d1
deathstroke-Assassin <moh2004amed@gmail.com>
2023-12-20 12:25:38 -0800

    llll

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,23 +1,46 @@
 #!/usr/bin/python3
-""" creates class Square """
+"""creates class Square."""
 
 
 class Square:
-    """ Square class"""
-    def __init__(self, size=0):
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+    """ Square class defined
+        Attributes:
+            size (int): Size of square
+            position (tuple): position of space and new lines
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        """initializes
+        Args:
+            size (int): size
+            postion(tuple): postion
+        Returns:
+            None
+        """
+
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
+        """
+        getter of size
+        Return:
+            Size of square
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
+        """
+        Setter of size
+        Args:
+            value (int): size
+        Raises
+            TypeError: if size is not int
+            ValueError: size less than 0
+        Returns:
+            None
+        """
         if type(value) != int:
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -25,15 +48,6 @@ class Square:
         else:
             self.__size = value
 
-    def area(self):
-        return self.__size * self.__size
-
-    def my_print(self):
-        if self.__size == 0:
-            print('')
-        for u in range(self.__size):
-            print("#" * self.__size)
-            u += 1
     @property
     def position(self):
         """
