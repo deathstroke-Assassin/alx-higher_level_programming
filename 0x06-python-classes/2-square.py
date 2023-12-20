72517ca64adec02af0e58cc9fafa75c3a9e79db3
deathstroke-Assassin <moh2004amed@gmail.com>
2023-12-20 02:51:59 -0800

    kkl

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,7 +6,7 @@ class Square:
     """ empty class for task 0 """
     def __init__(self, size=0):
         """ Initialize class """
-        if type(size) =! int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
