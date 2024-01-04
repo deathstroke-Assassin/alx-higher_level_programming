79ef599e0bd10972e558788a9b174f107f4e37e4
deathstroke-Assassin <moh2004amed@gmail.com>
2024-01-04 10:04:00 -0800

    llc

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -45,4 +45,6 @@ class Rectangle:
 
     def perimeter(self):
         """per"""
+        if self.__width == 0 or self.__height == 0:
+            return (0)
         return (self.__width + self.__height) * 2
