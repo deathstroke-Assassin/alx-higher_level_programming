5dcf0cbad3d4db2d61e0237cec28112b76ffeb76
deathstroke-Assassin <moh2004amed@gmail.com>
2024-01-04 10:01:37 -0800

    ll

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+Rectangle
+"""
+
+
+class Rectangle:
+    """pass it"""
+    def __init__(self, width=0, height=0):
+        """init"""
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """nn"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """kk"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """x"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ll"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Area"""
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """per"""
+        return (self.__width + self.__height) * 2
