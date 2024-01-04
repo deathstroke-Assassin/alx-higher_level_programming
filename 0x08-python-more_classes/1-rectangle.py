cd6f1ef12621d001ce02c6449d3534811ccbea24
deathstroke-Assassin <moh2004amed@gmail.com>
2024-01-04 09:36:01 -0800

    ccc

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Rectangle
+"""
+
+
+class Rectangle:
+    """pass it"""
+    def __init__(self, width=0, height=0):
+        """init"""
+        self.__height = height
+        self.__width = width
+
+    @property
+    def width(self):
+        """nn"""
+        return self.__width
+
+    @width_setter
+    def width(self, value):
+        """kk"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """x"""
+        return self.__height
+
+    @height_setter
+    def height(self, value):
+        """ll"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
