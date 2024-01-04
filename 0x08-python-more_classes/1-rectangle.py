c5659f39f7c5f30d5b607dd5654f0a59def3a36c
deathstroke-Assassin <moh2004amed@gmail.com>
2024-01-04 09:42:13 -0800

    jj

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,8 +8,8 @@ class Rectangle:
     """pass it"""
     def __init__(self, width=0, height=0):
         """init"""
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
