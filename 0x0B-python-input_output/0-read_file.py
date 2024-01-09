a2836a0bf6d30f3ca741077c529820f45c134909
deathstroke-Assassin <moh2004amed@gmail.com>
2024-01-09 11:47:20 -0800

    sd

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+    0-read_file: read_file()
+"""
+
+
 def read_file(filename=""):
+    """
+        read_file reads teaxt file and prints to stdout
+    """
     with open(filename, "r", encoding="utf-8") as f:
         print("{}".format(f.read()), end="")
