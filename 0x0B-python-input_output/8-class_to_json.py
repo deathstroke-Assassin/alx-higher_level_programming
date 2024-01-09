b70abc1f54b91a415142d8fb598b393156caa324
deathstroke-Assassin <moh2004amed@gmail.com>
2024-01-09 13:02:06 -0800

    n

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    0-read_file: read_file()
+"""
+
+
+def class_to_json(obj):
+    """
+        read_file reads teaxt file and prints to stdout
+    """
+    return obj.__dict__
