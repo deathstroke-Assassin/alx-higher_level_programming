ff45cb84d2de2793861332c26fa98dbfb4ae2cfb
deathstroke-Assassin <moh2004amed@gmail.com>
2024-03-11 20:07:59 -0700

    kj

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const index = process.argv.length;
+
+// print process.argv
+if (index === 2) {
+	console.log('No argument');
+}
+else if (index === 3) {
+	console.log('argument found');
+}
+else {
+	console.log('arguments found');
+}
